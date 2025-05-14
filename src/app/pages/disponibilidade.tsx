124f2eba10e07c88f7aa6ad14b40cb4eb587e59b
Eduardo Netto Freyer <eduardofreyer@hotmail.com>
2025-05-13 21:10:32 -0300

    Implementação do calendário

--- src/app/pages/disponibilidade.tsx
+++ src/app/pages/disponibilidade.tsx
@@ -4,35 +4,38 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import esLocale from "@fullcalendar/core/locales/pt-br";
 
-// import NavBar from "../components/navBar";
-// import TopBar from "../components/topBar";
+import NavBar from "../components/navBar";
+import TopBar from "../components/topBar";
 
 export default function Disponibilidade() {
   return (
     <>
-      {/* <NavBar />
-      <TopBar title="Disponibilidade" /> */}
+      <NavBar />
+      <TopBar title="Disponibilidade" />
 
-      <main className="flex flex-col min-h-screen justify-center items-center p-24">
-        <div className="grid grid-cols-10">
-          <div className="col-span-18">
+      <main className="flex flex-col min-h-screen justify-center items-center p-0">
+        <div className="flex justify-center items-center w-full">
+          <div className="ml-[288px] mt-28 absolute">
             <FullCalendar
               plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
               headerToolbar={{
-                left: "prev,next today",
+                start: "prev,next today",
                 center: "title",
-                right:
-                  "resourceTimelineWook, dayGridMonth,timeGridWeek,timeGridDay",
+                end: "dayGridMonth,timeGridWeek,timeGridDay",
               }}
               events={{}}
               nowIndicator={true}
               editable={true}
               selectable={true}
               selectMirror={true}
+              locale={esLocale}
               // dateClick={{}}
               // drop={}
               // eventClick={}
+              aspectRatio={1.1}
+              contentHeight={500}
             />
           </div>
         </div>
