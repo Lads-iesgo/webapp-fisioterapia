6213d6333eeab9d0fae4b25e4980b35b97a16391
Kevin Lima Garza <kevin@ad.local>
2025-05-15 09:56:00 -0300

    Criação do front de cadastro de usuário

---
+++ src/app/pages/CadastroUsuario.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/navBar";
+import TopBar from "../components/topBar";
+
+export default function Home() {
+  return (
+    <div className="bg-white h-screen flex flex-row">
+      <NavBar />
+      <TopBar title="Cadastrar Usuário" />
+    </div>
+  );
+}
