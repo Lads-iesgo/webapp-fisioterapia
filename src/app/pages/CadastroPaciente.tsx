b6b4e4edfe30a74536ccb92abe8bea1a8db2cae5
Kevin Lima Garza <kevin@ad.local>
2025-05-19 09:21:04 -0300

    Adicionei a tela de cadastro de usuário

--- src/app/pages/CadastroPaciente.tsx
+++ src/app/pages/CadastroPaciente.tsx
@@ -3,33 +3,33 @@
 import NavBar from "../components/navBar";
 import TopBar from "../components/topBar";
 
-export default function CadastrarUsuário() {
+export default function CadastrarPaciente() {
   return (
     <div className="bg-white ">
       {/* Sidebar */}
       <NavBar />
 
       {/* Área principal */}
-      <div className="flex flex-col flex-grow min-h-screen ml-[288px] mt-11">
+      <div className="flex flex-col flex-grow ml-[288px] mt-11">
         {/* Topbar */}
-        <TopBar title="Cadastro de Usuário" />
+        <TopBar title="Cadastrar Paciente" />
 
         {/* Conteúdo centralizado horizontalmente */}
-        <div className="flex justify-center p-6 pt-20">
+        <div className="flex justify-center p-6">
           <div className="bg-white border rounded-xl shadow-md w-full max-w-4xl">
             {/* Cabeçalho visual */}
-            <div className="bg-blue-950 h-12 rounded-t-xl" />
+            <div className="bg-blue-950 h-13 rounded-t-xl" />
 
             {/* Formulário */}
             <form className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
-                <label className="block font-semibold text-gray-950">Nome</label>
+                <label className="block font-semibold text-gray-950">Nome Completo</label>
                 <input type="text" placeholder="Fulano" className="w-full border rounded px-3 py-2 text-gray-500" />
               </div>
 
               <div>
-                <label className="block font-semibold text-gray-950">Sobrenome</label>
-                <input type="text" placeholder="De Souza" className="w-full border rounded px-3 py-2 text-gray-500" />
+                <label className="block font-semibold text-gray-950">Data de Nascimento</label>
+                <input type="text" placeholder="00/00/0000" className="w-full border rounded px-3 py-2 text-gray-500" />
               </div>
 
               <div>
@@ -38,29 +38,32 @@ export default function CadastrarUsuário() {
               </div>
 
               <div>
-                <label className="block font-semibold text-gray-950">Semestre</label>
+                <label className="block font-semibold text-gray-950">Sexo</label>
                 <select className="w-full border rounded px-3 py-2 text-gray-500">
-                  <option>1° Semestre</option>
-                  <option>2° Semestre</option>
-                  <option>3° Semestre</option>
-                  <option>4° Semestre</option>
-                  <option>5° Semestre</option>
-                  <option>6° Semestre</option>
-                  <option>7° Semestre</option>
-                  <option>8° Semestre</option>
-                  <option>9° Semestre</option>
-                  <option>10° Semestre</option>
+                  <option>Não informar</option>
+                  <option>Masculino</option>
+                  <option>Feminino</option>
                 </select>
-              </div>
-
-              <div>
-                <label className="block font-semibold text-gray-950">Matrícula</label>
-                <input type="number" placeholder="12345" className="w-full border rounded px-3 py-2 text-gray-500" />
               </div>
 
               <div>
                 <label className="block font-semibold text-gray-950">Telefone</label>
                 <input type="text" placeholder="(99) 99999-9999" className="w-full border rounded px-3 py-2 text-gray-500" />
+              </div>
+
+              <div>
+                <label className="block font-semibold text-gray-950">CPF</label>
+                <input type="text" placeholder="123.456.789-00" className="w-full border rounded px-3 py-2 text-gray-500" />
+              </div>
+
+              <div>
+                <label className="block font-semibold text-gray-950">CEP</label>
+                <input type="text" placeholder="12345-678" className="w-full border rounded px-3 py-2 text-gray-500" />
+              </div>
+
+              <div className="md:col-span-2">
+                <label className="block font-semibold text-gray-950">Endereço</label>
+                <input type="text" placeholder="Rua Arduino de Moura" className="w-full border rounded px-3 py-2 text-gray-500" />
               </div>
 
               {/* Botões */}
