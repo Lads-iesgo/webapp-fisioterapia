3fc44342e194b59ddf60763d73a93b7c708751fd
Halbert Nascimento <halbert.fac@gmail.com>
2025-05-30 21:43:42 -0300

    adiciona CookiesProvider para gerenciamento de cookies no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NotificationProvider } from "./components/Notification";
+import { CookiesProvider } from "next-client-cookies/server"; // Corrigir importação
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NotificationProvider>
-          {children}
-        </NotificationProvider>
+        <CookiesProvider>
+          <NotificationProvider>
+            {children}
+          </NotificationProvider>
+        </CookiesProvider>
       </body>
     </html>
   );
