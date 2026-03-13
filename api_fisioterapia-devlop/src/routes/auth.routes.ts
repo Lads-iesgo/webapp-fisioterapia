1108c0a2585f536cabac15839c7ac65bde5521d7
Josué Guimarães <132490266+JosueGuimaraes@users.noreply.github.com>
2026-03-13 20:07:17 -0300

    Delete api_fisioterapia-devlop directory
    .
    
    Signed-off-by: Josué Guimarães <132490266+JosueGuimaraes@users.noreply.github.com>

--- api_fisioterapia-devlop/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login } from "../controller/authController";
-
-const router = express.Router();
-
-// Rota de login
-router.post("/login", login);
-
-export default router;
