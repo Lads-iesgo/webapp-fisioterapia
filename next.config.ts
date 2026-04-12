1b2311dbf88ed080784994a49645bab246eef36f
Eduardo Netto Freyer <51035176+EduardoFreyer@users.noreply.github.com>
2026-04-12 00:06:06 -0300

    Correção para o Docker

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const dynamicOrigins = (process.env.NEXT_DEV_ORIGINS || "")
+	.split(",")
+	.map((o) => o.trim())
+	.filter(Boolean);
+
 const nextConfig = {
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    // "http://192.168.15.10:3000", // coloque aqui o IP que você usa para acessar
-  ],
+	allowedDevOrigins: [
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+		"http://clinica.iesgo.edu.br:9003",
+		"https://clinica.iesgo.edu.br:9003",
+		"http://72.60.142.42:9003/",
+		"https://72.60.142.42:9003/",
+		...dynamicOrigins,
+	],
 };
 
 module.exports = nextConfig;
