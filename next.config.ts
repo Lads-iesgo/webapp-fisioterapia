6ae7fb89609bc9261fc0f2d92ce8f7a0af44411e
Lads-iesgo <lads@iesgo.edu.br>
2026-04-12 00:09:38 -0300

    Correção Docker
    Signed-off-by: Lads-iesgo <lads@iesgo.edu.br>

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,7 @@ const dynamicOrigins = (process.env.NEXT_DEV_ORIGINS || "")
 
 const nextConfig = {
 	allowedDevOrigins: [
-		"http://localhost:3000",
-		"http://127.0.0.1:3000",
-		"http://clinica.iesgo.edu.br:9003",
-		"https://clinica.iesgo.edu.br:9003",
-		"http://72.60.142.42:9003/",
-		"https://72.60.142.42:9003/",
+		"*",
 		...dynamicOrigins,
 	],
 };
